2e0fdeeba17a06624c0a490475f8f172542c9e51
Jonathan Stowe <jns@gellyfish.co.uk>
2016-10-03 12:04:11 +0100

    Initial commit

---
+++ Tinky-Sharp/Tinky.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Tinky {
+	public class State {
+
+		public string name {
+			get;
+			set;
+		}
+		
+		public State () {
+
+		}
+	}
+
+	public class Transition {
+		public Transition () {
+		}
+	}
+}
+
